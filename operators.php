a37f32167a22baabc3cbf79449d1f631da13d2ba
WilliamBechtol <WillBechtol07@gmail.com>
2025-03-31 09:34:45 -0500

    operators

---
+++ operators.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //Arithmetic Operators
+    echo 5+5;
+    echo 5-5;
+    echo 5*5;
+    echo 5/5;
+    echo 8%5;
+    echo 5**2;
+    //Variable Operators
+    $x = 100;
+    $x /= 20;
+    //Comparison Operators
+    echo $x;
+    $x = 5;
+    $y = 5;
+    if ($x == $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+    $x = 5;
+    $y = 5;
+    if ($x != $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+    $x = 5;
+    $y = "5";
+    if ($x === $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+    $x = 10;
+    $y = 5;
+    if ($x > $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+    $x = 5;
+    $y = 10;
+    if ($x < $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+    $x = 10;
+    $y = 10;
+    if ($x <= $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+    $x = 10;
+    $y = 10;
+    if ($x >= $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+    $x = 10;
+    $y = 10;
+    if ($x <> $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+    //Increment/Decrement Operators
+    $x = 10;
+    echo $x++;
+    echo $x;
+    $x = 10;
+    echo $x--;
+    echo $x;
+    //Logical Operators
+    $x = 10;
+    $y = 20;
+
+    if ($x == $y or 1 == 1) {
+        echo "True";
+    }
+    $x = 20;
+    $y = 20;
+
+    if ($x == $y and 1 == 1) {
+        echo "True";
+    }
+    $x = 10;
+    $y = 20;
+
+    if ($x == $y xor 1 == 1) {
+        echo "True";
+    }
+    ?>
+</body>
+</html>
