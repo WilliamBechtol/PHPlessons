5a0de3dabbed953e949fdcc4569a462f29d69310
WilliamBechtol <WillBechtol07@gmail.com>
2025-03-31 09:54:21 -0500

    switch

--- conditionals.php
+++ conditionals.php
@@ -25,5 +25,25 @@
         echo "Daniel is very ugly!";
     }
     ?>
+    <?php
+    $x = 8;
+
+    switch ($x) {
+        case 1:
+            echo "the answer is 1";
+        break;
+        case "2":
+            echo "the answer is 2";
+        break;
+        case "3":
+            echo "the answer is 3";
+        break;
+        case "4":
+            echo "the answer is 4";
+        break;
+        default:
+            echo "There is no answer";
+    }
+    ?>
 </body>
 </html>
