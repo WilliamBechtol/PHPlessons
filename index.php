9ab118bff684d70003db83799455d0671be2ecea
WilliamBechtol <WillBechtol07@gmail.com>
2025-03-31 09:45:47 -0500

    conditionals

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
   <a href="datatypes.php">datatypes</a>
   <br>
   <a href="operators.php">operators</a>
+  <br>
+  <a href="conditionals.php">conditionals</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
