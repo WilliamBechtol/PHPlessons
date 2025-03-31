dcda1983cbf1c52b264a66639724da0f167cea53
WilliamBechtol <WillBechtol07@gmail.com>
2025-03-31 09:01:44 -0500

    data

---
+++ datatypes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //String
+    $name = 'Coding is fun';
+
+    //Integer
+    $name = 20;
+
+    //Float
+    $name = 20.5784;
+
+    /*Boolean
+    true = 1
+    false = 0*/
+
+    //Array
+    $names = array("Daniel","Dennis","Michael");
+    echo $names['2']
+    ?>
+</body>
+</html>
