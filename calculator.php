361feb1888393cac0855eb2f9ba7b8e89d6bab7c
WilliamBechtol <WillBechtol07@gmail.com>
2025-04-11 09:10:31 -0500

    Navbar adaptation

--- calculator.php
+++ calculator.php
@@ -1,52 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
-<body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">PHP</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.php">Index</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="operators.php">Operators</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="datatypes.php">Datatypes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="conditionals.php">Conditionals</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="calculator.php">Calculator</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="calendar.php">Calendar</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="loops.php">Loops</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="arrays.php">Array</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="functions.php">Functions</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php
+  include 'C:\xampp\htdocs\PHPlessons\includes\header.php';
+?>
     <form action="">
         <input type="text" name="num1" placeholder="Number 1">
         <input type="text" name="num2" placeholder="Number 2">
