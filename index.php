3d80e6eb076a5c18db9a80a1df8e315d6d351a54
WilliamBechtol <WillBechtol07@gmail.com>
2025-04-22 09:42:32 -0500

    connect

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
   include 'C:\xampp\htdocs\PHPlessons\includes\header.php';
 ?>
+<?php
+  $sql = "SELECT * FROM users WHERE username='dilly';";
+  $result = mysqli_query($conn, $sql);
+  $resultCheck = mysqli_num_rows($result);
+
+  if ($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<br>'.$row['username'];
+    }
+  }
+?>
   <form method="GET">
       <input type="text" name="person">
       <button class="btn btn-dark">SUBMIT</button>
